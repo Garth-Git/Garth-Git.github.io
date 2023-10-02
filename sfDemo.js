af5ec834d59a6880c6c5582c7c78e2dc26a704f4
Garth-Git <78998942+Garth-Git@users.noreply.github.com>
2023-10-02 12:35:39 +0200

    Update sfDemo.js

--- sfDemo.js
+++ sfDemo.js
@@ -6,13 +6,13 @@
     const footerBtn = document.createElement("ui5-button");
     const footerBtn2 = document.createElement("ui5-button");
     // set attributes
-    dialog.setAttribute("header-text", "Popup");
+    dialog.setAttribute("header-text", "Sign Up Today");
     footer.setAttribute("slot", "footer");
     // content
     content.innerHTML = "<h2>Sign Up</h2><p>Sign up for our newsletter</p><form>  <label for='fname'>Email Address:</label><br><input type='text' id='email' name='email' value='john.doe@demo.com'><br><br><label for='animal'>Favourite animal:</label><br><select id='animal' name='animal'><option value='rhino'>Rhino</option><option value='elephant'>Elephant</option><option value='lion'>Lion</option><option value='giraffe'>Griaffe</option></select><br><br></form>";
     content.style.padding = "1rem";
     // footer btn
-    footerBtn.onclick = () => alert('Thank you!');
+    footerBtn.onclick = () => { alert('Thank you!'); setTimeout("document.getElement('ui5-dialog').close();", 3000); }
     footerBtn.innerHTML = "Submit";
     footerBtn2.onclick = () => dialog.close();
     footerBtn2.innerHTML = "Close";
